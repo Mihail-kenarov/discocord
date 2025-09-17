fbf49dbbf907177b003e962f05ca6c37bde8eaca
Kenarov,Mihail M.M. <m.kenarov@student.fontys.nl>
2025-09-17 10:55:11 +0200

    Clerk signUp/SignIn is in the system now

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,15 @@
-"use client";
-
 import Image from "next/image";
 
-
 export default function Home() {
-
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
-      {/* Left side - image */}
-      <div className="relative hidden md:block">
-        <Image
-          src="/landing_boi.jpg"
-          alt="Landing illustration"
-          fill
-          className="object-cover"
-          priority
-        />
-      </div>
-
-      {/* Right side - canvas */}
-      <div className="flex items-center justify-center bg-white p-6">
-    
-    
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <Image
+        src="/landing_landscape.jpg"
+        alt="Landing Landscape"
+        width={1920}
+        height={1080}
+        className="object-cover"
+      />
     </div>
   );
 }
