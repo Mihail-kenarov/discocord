b95a5cad4c77f6a92a2bee1931d3015a22a410b6
Kenarov,Mihail M.M. <m.kenarov@student.fontys.nl>
2025-10-13 11:18:55 +0200

    test new runner

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -34,7 +34,6 @@ export async function POST(req: NextRequest) {
       }
     }
 
-    
     return new Response("Webhook processed successfully", { status: 200 });
   } catch (err) {
     console.error("Error verifying webhook:", err);
