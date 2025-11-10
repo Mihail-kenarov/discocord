92ded717e82677e2b159346f509f6f4c5eabf252
Kenarov,Mihail M.M. <m.kenarov@student.fontys.nl>
2025-11-10 09:38:10 +0100

    videos of the e2e tests passing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true,
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    video: false,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // your node event listeners
     },
   },
 });
